2e1ff244af845698e77f861bb9af785ed54b0e2e
Edwin Ade <edwinmbonyjr@gmail.com>
2026-04-23 17:12:46 +0100

    feat: create classifier functionality

---
+++ internal/classify/classify.go
@@ -0,0 +1,54 @@
+package classify
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/valentino7504/file-classifier-go/internal/proc"
+)
+
+func moveFile(src string, dest string) error {
+	finalPath, ext := dest, filepath.Ext(dest)
+	for i := 1; ; i++ {
+		if _, err := os.Stat(finalPath); os.IsNotExist(err) {
+			break
+		} else if err != nil {
+			return err
+		}
+		finalPath = fmt.Sprintf("%s(%d)%s", strings.TrimSuffix(dest, ext), i, ext)
+	}
+	return os.Rename(src, finalPath)
+}
+
+func Classify(basePath string, openFiles map[string]bool) {
+	for _, folder := range folders {
+		err := os.MkdirAll(filepath.Join(basePath, folder), 0o755)
+		if err != nil {
+			return
+		}
+	}
+	entries, err := os.ReadDir(basePath)
+	if err != nil {
+		return
+	}
+	for _, entry := range entries {
+		entryName := entry.Name()
+		if entry.IsDir() || strings.HasSuffix(entryName, ".part") {
+			continue
+		}
+		filePath := filepath.Join(basePath, entryName)
+		if !proc.IsAvailable(filePath, openFiles) {
+			continue
+		}
+		ext := strings.ToLower(filepath.Ext(entryName))
+		if dir, ok := extensions[ext]; ok {
+			destPath := filepath.Join(basePath, dir, entryName)
+			err = moveFile(filePath, destPath)
+			if err != nil {
+				continue
+			}
+		}
+	}
+}
